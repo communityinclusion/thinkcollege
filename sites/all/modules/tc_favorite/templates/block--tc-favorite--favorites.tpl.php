ae1a7ec6ea4e0db9111eecc9bca74ecb5b118ce7
Michael Anello <michael@anelloconsulting.com>
2016-11-28 10:22:13 -0500

    Template-ized the custom Favorites block.

---
+++ sites/all/modules/tc_favorite/templates/block--tc-favorite--favorites.tpl.php
@@ -0,0 +1,34 @@
+<?php
+
+$favorites = $variables['favorites'];
+$emailform = $variables['emailform'];
+
+if (count($favorites)) {
+  foreach($favorites as $nid => $title) {
+    $titlelink = l($title, 'node/'. $nid);
+    ?>
+    <div class="saved"><p><a href="/collegesearch/favorite/remove/<?php print $nid ?>?destination=<?php print current_path() ?>">X</a> <?php print $titlelink ?></p></div>
+    <?php
+  }
+  ?>
+
+  <div class="btn-group">
+    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#tc_favorite_email_modal">Email</button>
+    <a type="button" class="btn btn-default">Print</a>
+  </div>
+
+  <div class="modal fade" id="tc_favorite_email_modal" role="dialog">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">×</button>
+          <h4>Email saved programs</h4>
+        </div>
+        <div class="modal-body">
+             <?php print drupal_render($emailform); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php
+}
