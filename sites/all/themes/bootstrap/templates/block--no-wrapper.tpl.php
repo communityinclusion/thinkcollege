52326b417213c4d9ba8a3fe7d6c5ad39308bad3f
jrepo <julie.reposa@gmail.com>
2017-01-31 15:19:19 -0500

    more block theming

--- sites/all/themes/bootstrap/templates/block--no-wrapper.tpl.php
+++ sites/all/themes/bootstrap/templates/block--no-wrapper.tpl.php
@@ -1,6 +1,6 @@
 <?php print render($title_prefix); ?>
 <?php if ($title): ?>
-  <h3<?php print $title_attributes; ?>><?php print $title; ?></h3>
+  <h2<?php print $title_attributes; ?>><?php print $title; ?></h2>
 <?php endif; ?>
 <?php print render($title_suffix); ?>
 
