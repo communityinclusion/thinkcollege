d8ecb3ec68ba8d7cd4ff3f123b3d9c77345d1ed8
Lee Walker <lee@codejourneymen.com>
2017-02-14 16:45:58 -0500

    add breadcrumb theming for search
    Signed-off-by: Lee Walker <lee@codejourneymen.com>

--- sites/all/themes/thinkcollege_boot/template.php
+++ sites/all/themes/thinkcollege_boot/template.php
@@ -5,133 +5,143 @@
  * template.php
  */
 
+/**
+ * Implements theme_form_element().
+ *
+ * @param $variables
+ * @return string
+ */
+function thinkcollege_boot_form_element(&$variables) {
+  $element = &$variables['element'];
+  // print_r($element);
+  $is_checkbox = FALSE;
+  $is_radio = FALSE;
 
- function thinkcollege_boot_form_element(&$variables) {
-   $element = &$variables['element'];
-	// print_r($element);
-   $is_checkbox = FALSE;
-   $is_radio = FALSE;
+  // This function is invoked as theme wrapper, but the rendered form element
+  // may not necessarily have been processed by form_builder().
+  $element += array(
+    '#title_display' => 'before',
+  );
 
-   // This function is invoked as theme wrapper, but the rendered form element
-   // may not necessarily have been processed by form_builder().
-   $element += array(
-     '#title_display' => 'before',
-   );
+  // Add element #id for #type 'item'.
+  if (isset($element['#markup']) && !empty($element['#id'])) {
+    $attributes['id'] = $element['#id'];
+  }
 
-   // Add element #id for #type 'item'.
-   if (isset($element['#markup']) && !empty($element['#id'])) {
-     $attributes['id'] = $element['#id'];
-   }
+  // Check for errors and set correct error class.
+  if (isset($element['#parents']) && form_get_error($element)) {
+    $attributes['class'][] = 'error';
+  }
 
-   // Check for errors and set correct error class.
-   if (isset($element['#parents']) && form_get_error($element)) {
-     $attributes['class'][] = 'error';
-   }
+  if (!empty($element['#type'])) {
+    $attributes['class'][] = 'form-type-' . strtr($element['#type'], '_', '-');
+  }
+  if (!empty($element['#name'])) {
+    $attributes['class'][] = 'form-item-' . strtr($element['#name'], array(
+        ' ' => '-',
+        '_' => '-',
+        '[' => '-',
+        ']' => '',
+      ));
+  }
+  // Add a class for disabled elements to facilitate cross-browser styling.
+  if (!empty($element['#attributes']['disabled'])) {
+    $attributes['class'][] = 'form-disabled';
+  }
+  if (!empty($element['#autocomplete_path']) && drupal_valid_path($element['#autocomplete_path'])) {
+    $attributes['class'][] = 'form-autocomplete';
+  }
+  $attributes['class'][] = 'form-item';
 
-   if (!empty($element['#type'])) {
-     $attributes['class'][] = 'form-type-' . strtr($element['#type'], '_', '-');
-   }
-   if (!empty($element['#name'])) {
-     $attributes['class'][] = 'form-item-' . strtr($element['#name'], array(
-         ' ' => '-',
-         '_' => '-',
-         '[' => '-',
-         ']' => '',
-       ));
-   }
-   // Add a class for disabled elements to facilitate cross-browser styling.
-   if (!empty($element['#attributes']['disabled'])) {
-     $attributes['class'][] = 'form-disabled';
-   }
-   if (!empty($element['#autocomplete_path']) && drupal_valid_path($element['#autocomplete_path'])) {
-     $attributes['class'][] = 'form-autocomplete';
-   }
-   $attributes['class'][] = 'form-item';
+  // See http://getbootstrap.com/css/#forms-controls.
+  if (isset($element['#type'])) {
+    if ($element['#type'] == "radio") {
+      $attributes['class'][] = 'radio';
+      $is_radio = TRUE;
+    }
+    elseif ($element['#type'] == "checkbox") {
+      $attributes['class'][] = 'checkbox';
+      $is_checkbox = TRUE;
+    }
+    else {
+      $attributes['class'][] = 'form-group';
+    }
+  }
 
-   // See http://getbootstrap.com/css/#forms-controls.
-   if (isset($element['#type'])) {
-     if ($element['#type'] == "radio") {
-       $attributes['class'][] = 'radio';
-       $is_radio = TRUE;
-     }
-     elseif ($element['#type'] == "checkbox") {
-       $attributes['class'][] = 'checkbox';
-       $is_checkbox = TRUE;
-     }
-     else {
-       $attributes['class'][] = 'form-group';
-     }
-   }
+  $description = FALSE;
+  $tooltip = FALSE;
+  // Convert some descriptions to tooltips.
+  // @see bootstrap_tooltip_descriptions setting in _bootstrap_settings_form()
+  if (!empty($element['#description']) && $element['#parents'][0] != 'radio_cols') {
+    $description = $element['#description'];
+    if (theme_get_setting('bootstrap_tooltip_enabled') && theme_get_setting('bootstrap_tooltip_descriptions') && $description === strip_tags($description) && strlen($description) <= 200) {
+      $tooltip = TRUE;
+      $attributes['data-toggle'] = 'tooltip';
+      $attributes['title'] = $description;
+    }
+  } else {if (!empty($element['#description'])) $description = $element['#description']; }
 
-   $description = FALSE;
-   $tooltip = FALSE;
-   // Convert some descriptions to tooltips.
-   // @see bootstrap_tooltip_descriptions setting in _bootstrap_settings_form()
-   if (!empty($element['#description']) && $element['#parents'][0] != 'radio_cols') {
-     $description = $element['#description'];
-     if (theme_get_setting('bootstrap_tooltip_enabled') && theme_get_setting('bootstrap_tooltip_descriptions') && $description === strip_tags($description) && strlen($description) <= 200) {
-       $tooltip = TRUE;
-       $attributes['data-toggle'] = 'tooltip';
-       $attributes['title'] = $description;
-     }
-   } else {if (!empty($element['#description'])) $description = $element['#description']; }
+  $output = '<div' . drupal_attributes($attributes) . '>' . "\n";
 
-   $output = '<div' . drupal_attributes($attributes) . '>' . "\n";
+  // If #title is not set, we don't display any label or required marker.
+  if (!isset($element['#title'])) {
+    $element['#title_display'] = 'none';
+  }
 
-   // If #title is not set, we don't display any label or required marker.
-   if (!isset($element['#title'])) {
-     $element['#title_display'] = 'none';
-   }
+  $prefix = '';
+  $suffix = '';
+  if (isset($element['#field_prefix']) || isset($element['#field_suffix'])) {
+    // Determine if "#input_group" was specified.
+    if (!empty($element['#input_group'])) {
+      $prefix .= '<div class="input-group">';
+      $prefix .= isset($element['#field_prefix']) ? '<span class="input-group-addon">' . $element['#field_prefix'] . '</span>' : '';
+      $suffix .= isset($element['#field_suffix']) ? '<span class="input-group-addon">' . $element['#field_suffix'] . '</span>' : '';
+      $suffix .= '</div>';
+    }
+    else {
+      $prefix .= isset($element['#field_prefix']) ? $element['#field_prefix'] : '';
+      $suffix .= isset($element['#field_suffix']) ? $element['#field_suffix'] : '';
+    }
+  }
 
-   $prefix = '';
-   $suffix = '';
-   if (isset($element['#field_prefix']) || isset($element['#field_suffix'])) {
-     // Determine if "#input_group" was specified.
-     if (!empty($element['#input_group'])) {
-       $prefix .= '<div class="input-group">';
-       $prefix .= isset($element['#field_prefix']) ? '<span class="input-group-addon">' . $element['#field_prefix'] . '</span>' : '';
-       $suffix .= isset($element['#field_suffix']) ? '<span class="input-group-addon">' . $element['#field_suffix'] . '</span>' : '';
-       $suffix .= '</div>';
-     }
-     else {
-       $prefix .= isset($element['#field_prefix']) ? $element['#field_prefix'] : '';
-       $suffix .= isset($element['#field_suffix']) ? $element['#field_suffix'] : '';
-     }
-   }
+  switch ($element['#title_display']) {
+    case 'before':
+    case 'invisible':
+    $output .= ' ' . theme('form_element_label', $variables);
+    $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+    break;
 
-   switch ($element['#title_display']) {
-     case 'before':
-     case 'invisible':
-       $output .= ' ' . theme('form_element_label', $variables);
-       $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
-       break;
+    case 'after':
+      if ($is_radio || $is_checkbox) {
+        $output .= ' ' . $prefix . $element['#children'] . $suffix;
+      }
+      else {
+        $variables['#children'] = ' ' . $prefix . $element['#children'] . $suffix;
+      }
+      $output .= ' ' . theme('form_element_label', $variables) . "\n";
+      break;
 
-     case 'after':
-       if ($is_radio || $is_checkbox) {
-         $output .= ' ' . $prefix . $element['#children'] . $suffix;
-       }
-       else {
-         $variables['#children'] = ' ' . $prefix . $element['#children'] . $suffix;
-       }
-       $output .= ' ' . theme('form_element_label', $variables) . "\n";
-       break;
+    case 'none':
+    case 'attribute':
+    // Output no label and no required marker, only the children.
+    $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+    break;
+  }
 
-     case 'none':
-     case 'attribute':
-       // Output no label and no required marker, only the children.
-       $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
-       break;
-   }
+  if (($description && $element['#parents'][0] == 'radio_cols') || ($description && !$tooltip)) {
+    $output .= '<p class="help-block">' . $element['#description'] . "</p>\n";
+  }
 
-   if (($description && $element['#parents'][0] == 'radio_cols') || ($description && !$tooltip)) {
-     $output .= '<p class="help-block">' . $element['#description'] . "</p>\n";
-   }
+  $output .= "</div>\n";
 
-   $output .= "</div>\n";
-
-   return $output;
+  return $output;
 }
 
+/**
+ * Implements template_preprocess_page().
+ *
+ * @param $vars
+ */
 function thinkcollege_boot_preprocess_page(&$vars) {
   if (drupal_is_front_page()) {
     drupal_set_title(''); //removes welcome message (page title)
@@ -145,10 +155,9 @@ function thinkcollege_boot_preprocess_page(&$vars) {
   }
 }
 
-
-
-
 /**
+ * Implements template_preprocess_page().
+ *
  * Since we have a sidebar_first_top, we have to make sure that the content region
  * resizes properly when there is a sidebar_first_top but no sidebar_first content.
  *
@@ -168,16 +177,10 @@ function thinkcollege_boot_process_page(&$variables) {
   }
 }
 
-
-
-
-
-
-
-
-
-
-
+/**
+ * Implements template_preprocess_html().
+ * @param $vars
+ */
 function thinkcollege_boot_preprocess_html(&$vars) {
   $filepath = path_to_theme() . '/font-awesome/css/font-awesome.min.css';
   drupal_add_css($filepath, array(
@@ -193,7 +196,7 @@ function thinkcollege_boot_preprocess_html(&$vars) {
 }
 
 /**
- * Implements hook_preprocess_node().
+ * Implements template_preprocess_node().
  */
 function thinkcollege_boot_preprocess_node(&$vars) {
   $vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->type . '__' . $vars['view_mode'];
@@ -226,14 +229,14 @@ function thinkcollege_boot_preprocess_node(&$vars) {
 }
 
 /**
- * Implements hook_preprocess_field().
+ * Implements template_preprocess_field().
  */
 function thinkcollege_boot_preprocess_field(&$vars) {
   $vars['theme_hook_suggestions'][] = 'field__' . $vars['element']['#bundle'] . '__' . $vars['element']['#view_mode'];
 }
 
 /**
- * Implements hook_preprocess_block().
+ * Implements template_preprocess_block().
  */
 function thinkcollege_boot_preprocess_block(&$vars) {
   // Add Bootstrap "affix" settings for Program Record ScrollSpy menu block.
@@ -348,3 +351,45 @@ function thinkcollege_boot_facetapi_title($variables) {
   return $title . $tit . "</span></div>";
 }
 
+/**
+ * Implements theme_breadcrumb().
+ *
+ * Returns HTML for a breadcrumb trail.
+ *
+ * @param array $variables
+ *   An associative array containing:
+ *   - breadcrumb: An array containing the breadcrumb links.
+ *
+ * @return string
+ *   The constructed HTML.
+ *
+ * @see theme_breadcrumb()
+ *
+ * @ingroup theme_functions
+ */
+function thinkcollege_boot_breadcrumb($variables) {
+
+  // Use the Path Breadcrumbs theme function if it should be used instead.
+  if (_bootstrap_use_path_breadcrumbs()) {
+    return path_breadcrumbs_breadcrumb($variables);
+  }
+
+  $output = '';
+  $breadcrumb = $variables['breadcrumb'];
+
+  unset($breadcrumb[sizeof($breadcrumb)-1]);
+  array_splice($breadcrumb, 1, 0, '<a href="'.  base_path() . 'college-search" class="active">Think College Search</a>');
+
+  // Determine if we are to display the breadcrumb.
+  $bootstrap_breadcrumb = bootstrap_setting('breadcrumb');
+  if (($bootstrap_breadcrumb == 1 || ($bootstrap_breadcrumb == 2 && arg(0) == 'admin')) && !empty($breadcrumb)) {
+    $output = theme('item_list', array(
+      'attributes' => array(
+        'class' => array('breadcrumb'),
+      ),
+      'items' => $breadcrumb,
+      'type' => 'ol',
+    ));
+  }
+  return $output;
+}
