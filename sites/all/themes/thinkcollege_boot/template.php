0c79bc78703541adc45a0b1074a43ae0bb0d17ad
ICI User <paul.foos@umb.edu>
2017-03-02 14:31:20 -0500

    template fix

--- sites/all/themes/thinkcollege_boot/template.php
+++ sites/all/themes/thinkcollege_boot/template.php
@@ -1,4 +1,4 @@
-Cphp
+<?php
 
 /**
  * @file
@@ -378,7 +378,7 @@ function thinkcollege_boot_breadcrumb($variables) {
   $breadcrumb = $variables['breadcrumb'];
 
   // Special ordering in TC search.
-  if ((current_path() == "college-search") || (drupal_get_title() == "College Search") || ($base_url == 'programs.thinkcollege.net' && drupal_is_front_page())) {
+  if ((current_path() == "college-search") || (drupal_get_title() == "College Search")) {
     unset($breadcrumb[sizeof($breadcrumb) - 1]);
     array_splice($breadcrumb, 1, 0, '<a href="' . base_path() . 'college-search" class="active">Think College Search</a>');
   }
