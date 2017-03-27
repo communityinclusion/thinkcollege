46232f2f0f1194afa9980884e95b8d9e7404968a
Lee Walker <lee@codejourneymen.com>
2017-03-27 14:14:59 -0400

    attempt at a fix for facet blocks
    Signed-off-by: Lee Walker <lee@codejourneymen.com>

--- sites/all/modules/custom/tc_facets/templates/tc_program_features.tpl.php
+++ sites/all/modules/custom/tc_facets/templates/tc_program_features.tpl.php
@@ -5,19 +5,29 @@
 </h3>
 <?php
   // Dual Enrollment
-  $whole_block_de = block_load('facetapi','KA5QG3mUdAl4CWJKXaleJaI0xLV9mABI');
-  $renderable_array_de = _block_get_renderable_array(_block_render_blocks(array($whole_block_de)));
-  print drupal_render($renderable_array_de);
+
+//  $delta = facetapi_hash_delta(facetapi_build_delta('search_api@dev_solr_server', 'block', 'tc_dual_enroll'));
+  $delta = 'KA5QG3mUdAl4CWJKXaleJaI0xLV9mABI';
+  print render(module_invoke('facetapi', 'block_view', $delta));
+
+
+//  $whole_block_de = block_load('facetapi','ka5qg3MuDaL4cwjkxALEjAi0Xlv9Mabi');
+//  $renderable_array_de = _block_get_renderable_array(_block_render_blocks(array($whole_block_de)));
+//  print drupal_render($renderable_array_de);
 
   // Financial Aid
-  $whole_block_fa = block_load('facetapi','GKkN44mzr0b0a0Uvm7LabhsT3ymIHPyc');
-  $renderable_array_fa = _block_get_renderable_array(_block_render_blocks(array($whole_block_fa)));
-  print drupal_render($renderable_array_fa);
+//  $whole_block_fa = block_load('facetapi','GKkN44mzr0b0a0Uvm7LabhsT3ymIHPyc');
+//  $renderable_array_fa = _block_get_renderable_array(_block_render_blocks(array($whole_block_fa)));
+//  print drupal_render($renderable_array_fa);
+  $delta = 'GKkN44mzr0b0a0Uvm7LabhsT3ymIHPyc';
+  print render(module_invoke('facetapi', 'block_view', $delta));
 
   // length of programme
-  $whole_block_lp = block_load('facetapi','QvTDdqhn4mIrmS8bZMVB8KCXEQ6bq3YG');
-  $renderable_array_lp = _block_get_renderable_array(_block_render_blocks(array($whole_block_lp)));
-  print drupal_render($renderable_array_lp);
+//  $whole_block_lp = block_load('facetapi','c4QKVeEbjlARazcQU7OOaYqzQb56k9TF');
+//  $renderable_array_lp = _block_get_renderable_array(_block_render_blocks(array($whole_block_lp)));
+//  print drupal_render($renderable_array_lp);
+  $delta = 'c4QKVeEbjlARazcQU7OOaYqzQb56k9TF';
+  print render(module_invoke('facetapi', 'block_view', $delta));
 
   $x = sizeof($renderable_array_de);
   // A facet with no values will have 3 children, with renderable data 4 children.
