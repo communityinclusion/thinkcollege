5101a1ed1b0a319dd956e45f49def55c095f886a
Lee Walker <lee@codejourneymen.com>
2017-04-04 12:47:03 -0400

    note for facets
    Signed-off-by: Lee Walker <lee@codejourneymen.com>

--- sites/all/modules/custom/tc_facets/templates/tc_program_features.tpl.php
+++ sites/all/modules/custom/tc_facets/templates/tc_program_features.tpl.php
@@ -5,7 +5,6 @@
 </h3>
 <?php
   // Dual Enrollment
-
   $delta = 'KA5QG3mUdAl4CWJKXaleJaI0xLV9mABI';
   print render(module_invoke('facetapi', 'block_view', $delta));
 
@@ -17,6 +16,7 @@
   $delta = 'QvTDdqhn4mIrmS8bZMVB8KCXEQ6bq3YG';
   print render(module_invoke('facetapi', 'block_view', $delta));
 
+  // Only put Dual enrollment note on if dual enroll facet is shown.
   $x = module_invoke('facetapi', 'block_view', $delta);
   if ($x != null) {
   echo '<div class="aside">*Note that students who are dually enrolled might not be eligible for financial aid or housing</div>';
