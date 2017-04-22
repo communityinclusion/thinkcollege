096de514ed6073232e91925982d31f646a1d9ef9
Michael Anello <michael@anelloconsulting.com>
2017-04-22 13:02:53 -0400

    More Resource (teaser) theming.

--- sites/all/themes/thinkcollege_boot/templates/node/node--resource--teaser.tpl.php
+++ sites/all/themes/thinkcollege_boot/templates/node/node--resource--teaser.tpl.php
@@ -3,38 +3,37 @@ $classes .= ' node-resource-teaser node-tc-card';
 ?>
 <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
   <div class="resource-box">
+    <div class="topic-label clearfix">
+      <div class="topic pull-right">
+        <?php print render($content['field_resourc_publication_types']); ?>
+        <?php print render($content['field_resourc_topics']); ?>
+      </div>
+    </div>
 
-    <header>
-      <?php print render($title_prefix); ?>
-      <?php if (!$page && $title): ?>
-        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+    <div class="resource-title-listing">
+      <?php print render($content['field_resourc_date_received']); ?>
+      <?php print render($content['field_resourc_media_types']); ?>
+      <?php print l($title, '/node/' . $node->nid); ?>
+    </div>
+
+
+    <div class="resource-description">
+      <?php
+        // Hide comments, tags, and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+        hide($content['field_tags']);
+        print render($content);
+      ?>
+
+      <?php if (!empty($content['field_tags'])): ?>
+       <footer>
+          <?php print render($content['field_tags']); ?>
+       </footer>
       <?php endif; ?>
-      <?php print render($title_suffix); ?>
 
-      <?php if ($display_submitted): ?>
-        <span class="submitted">
-          <?php print $user_picture; ?>
-          <?php print $submitted; ?>
-        </span>
-      <?php endif; ?>
-    </header>
-
-    <?php
-      // Hide comments, tags, and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      hide($content['field_tags']);
-      print render($content);
-    ?>
-
-    <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
-     <footer>
-        <?php print render($content['field_tags']); ?>
-        <?php print render($content['links']); ?>
-     </footer>
-    <?php endif; ?>
-
-    <?php print render($content['comments']); ?>
+      <?php print render($content['comments']); ?>
+    </div>
   </div>
 
 </article> <!-- /.node -->
