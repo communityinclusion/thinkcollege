113922ef165caf5606d63a2b71376ac53dae096c
Michael Anello <michael@anelloconsulting.com>
2017-06-26 12:19:51 -0400

    Added full-width top row to tc_multipass layout (home page).

--- sites/all/modules/custom/tc_layouts/plugins/layouts/tc_multipass/tc-multipass.tpl.php
+++ sites/all/modules/custom/tc_layouts/plugins/layouts/tc_multipass/tc-multipass.tpl.php
@@ -13,6 +13,12 @@
 <div class="panel-display tc-multipass clearfix <?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
   <div class="row">
+    <div class="col-sm-12 tc-row0">
+      <?php print $content['row0']; ?>
+    </div>
+  </div>
+
+  <div class="row">
     <div class="col-md-8 tc-row1left">
       <?php print $content['row1left']; ?>
     </div>
