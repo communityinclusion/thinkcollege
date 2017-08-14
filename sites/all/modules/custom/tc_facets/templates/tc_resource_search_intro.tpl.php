55f57535b101f16e27edeaa3acbb276bd76325ab
Lee Walker <lee@codejourneymen.com>
2017-08-14 17:36:39 -0400

    resource into text now a seperate block
    Signed-off-by: Lee Walker <lee@codejourneymen.com>

---
+++ sites/all/modules/custom/tc_facets/templates/tc_resource_search_intro.tpl.php
@@ -0,0 +1,9 @@
+<p class="lead">Welcome to the Think College resource library!</p>
+<p>Here you will find hundreds of carefully selected resources on a wide range
+  of topics related to postsecondary education for people with intellectual
+  disabilities including transition, applying to and paying for college,
+  setting up and maintaining a college program, relevant legislation,
+  competitive employment, living in the community, accessing benefits, and more.</p>
+<p>Do you know of a resource that would be appropriate for our library?
+  Let us know! Email <a href="mailto:Rebecca.Lazo@umb.edu">Rebecca.Lazo@umb.edu</a>.
+<br/><br/>
