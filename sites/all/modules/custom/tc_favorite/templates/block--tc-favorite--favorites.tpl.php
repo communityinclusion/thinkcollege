96cf521c4e57dea092b9412e06a9ec3400597ad4
jrepo <julie.reposa@gmail.com>
2019-05-07 12:46:03 -0400

    updated text in favorites block

--- sites/all/modules/custom/tc_favorite/templates/block--tc-favorite--favorites.tpl.php
+++ sites/all/modules/custom/tc_favorite/templates/block--tc-favorite--favorites.tpl.php
@@ -37,7 +37,7 @@ if (count($favorites)) {
   <?php
 } else {
   ?>
-  <div>Click "<i class="fa fa-heart" aria-hidden="true"></i> save" for each program you want to save. You will be able to save, compare and download basic information for all your favorites. <br> See FAQ: <a href="/faq#savelist">How do I save and compare programs?</a></div>
+  <div>Click "<i class="fa fa-heart" aria-hidden="true"></i> save" to add programs to your favorites. You can then save, compare and download basic information for each one. <br> See FAQ: <a href="/faq#savelist">How do I save and compare programs?</a></div>
   <?php
 }
 
