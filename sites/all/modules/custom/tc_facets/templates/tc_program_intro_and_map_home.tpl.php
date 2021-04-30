41e4970906969abb37832a882c55b49fdc7c538d
jrepo <julie.reposa@gmail.com>
2021-04-30 15:46:43 -0400

    working on adding college search map to homepage

---
+++ sites/all/modules/custom/tc_facets/templates/tc_program_intro_and_map_home.tpl.php
@@ -0,0 +1,36 @@
+
+
+  <p>
+    This directory includes information on <span style="color:#20527C"><?php echo $count; ?></span>
+    college programs for students with intellectual disability. You can search by program name,
+     location, and other keywords. "Advanced Filters" offer more options to narrow your search.
+  </p>
+
+   <p>Review these <a title="Frequently Asked Questions" href="/faq">Frequently Asked Questions</a>
+     for answers to some general questions on college programs, how we gather the information
+     in the directory, and how to use the features of College Search.</p>
+
+<p>Once you have located colleges you are interested in, download
+  the <a href="/sites/default/files/files/resources/Conducting_College_Search_HTTC1v2.pdf">How To Think College Guide to Conducting a College Search [PDF]</a>.
+  It provides you with the right questions to ask in order to get the answers
+  you need about each program on your list. The How To guide is also <a href="/sites/default/files/files/resources/HowTo%20TC_spanish_FR.PDF">available in Spanish.</a></p>
+   <hr/>
+
+  <?php
+  include(drupal_get_path('module', 'tc_facets') . '/templates/tc_state_program_count_map.tpl.php');
+  ?>
+
+   <hr/>
+   <p class="small clearfix">
+    The information included here was submitted to Think College by the
+    college programs. Being listed here does not indicate or imply a
+    Think College endorsement. There also may be programs available that
+    have not reported to us. All programs listed here must be affiliated
+    with an accredited college or university and serve students with
+    intellectual disabilities. To let us know about a new program, or to
+    make changes to a current program listing,
+    please email <a href="mailto:thinkcollege@umb.edu">thinkcollege@umb.edu.</a>
+  </p>
+
+
+
