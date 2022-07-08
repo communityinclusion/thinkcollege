2b33a550f5a22ec9c26c6cde44363e2a979471da
Paul Foos <paul.foos@umb.edu>
2022-07-08 09:01:43 -0400

    changed text on Resources library intro page

--- sites/all/modules/custom/tc_facets/templates/tc_resource_search_intro.tpl.php
+++ sites/all/modules/custom/tc_facets/templates/tc_resource_search_intro.tpl.php
@@ -6,14 +6,8 @@
 <?php if ( !(array_key_exists('search_api_views_fulltext', $x))  && !(array_key_exists('f', $x))): ?>
   <div class="info-box">
     <p class="lead">Welcome to the Think College resource library!</p>
-    <p>Here you will find hundreds of carefully selected resources on a wide range of topics related to postsecondary education for people with intellectual disabilities. Topics include transition, applying to and paying for college, setting up and maintaining a college program, relevant legislation, competitive employment, living in the community, accessing benefits, and more.</p>
-
-    <p>Use the filters on the left to narrow your search by topic, resource type, audience, or related Think College project. Think College has several currently funded projects and numerous past projects. Each project has a specific focus and may have written or produced associated products. The designation of a specific project on a resource means that it was either created from funds from that project or it was most relevant to that project. You can find more information on all <a href="/about/what-is-think-college">Think College projects here</a>.</p>
-    <p>Here you will find hundreds of carefully selected resources on a wide range
-    of topics related to postsecondary education for people with intellectual
-    disabilities including transition, applying to and paying for college,
-    setting up and maintaining a college program, relevant legislation,
-    competitive employment, living in the community, accessing benefits, and more.</p>
-    <p>Do you know of a resource that would be appropriate for our library? Let us know! Email <a href="mailto:Rebecca.Lazo@umb.edu">Rebecca.Lazo@umb.edu</a>.</p>
+    <p> The library includes carefully selected resources on a wide range of topics related to postsecondary education for people with intellectual disability.
+</p><p>
+Use the filters on the left to narrow your search by topic, and click on Advanced Filters to refine by project, audience, media type, or publication type. If you are having trouble finding the resources you need, please contact us at <a href="mailto:thinkcollegeTA@gmail.com">thinkcollegeTA@gmail.com</a></p>
   </div>
 <?php endif; ?>
