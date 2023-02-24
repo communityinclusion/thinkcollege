648615215980614a473167f68ee90241f1b0d93a
Paul Foos <paul.foos@umb.edu>
2023-02-24 11:52:16 -0500

    front page slideshow id changed, not hiding unstyled content any more.  Updates in thinkcollege boot style.css and js

--- sites/all/themes/thinkcollege_boot/js/thinkcollege_boot.js
+++ sites/all/themes/thinkcollege_boot/js/thinkcollege_boot.js
@@ -37,7 +37,7 @@ var Drupal = Drupal || {};
     attach: function (context) {
       $(window).load(function() {
   // When the page has loaded
-  $('#views_slideshow_cycle_main_slideshow_test-block').fadeIn(1400);
+  $('#views_slideshow_cycle_main_slideshow_test-block_1').fadeIn(1400);
 });
     }
   };
