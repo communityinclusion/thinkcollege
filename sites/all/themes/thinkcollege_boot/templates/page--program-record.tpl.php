52efa03121ae7a27a3476e7aaa1a4f74dde3d97a
jrepo <julie.reposa@gmail.com>
2023-03-24 12:56:13 -0400

    tweaking utility nav

--- sites/all/themes/thinkcollege_boot/templates/page--program-record.tpl.php
+++ sites/all/themes/thinkcollege_boot/templates/page--program-record.tpl.php
@@ -76,7 +76,7 @@
 
   <?php if ($page['top_header']) : ?>
     <div id="top-header" class="top-header-region <?php print $top_header_classes; ?>">
-    <div class="container text-right">
+    <div class="container-fluid text-right">
         <?php if ($page['top_header']) { print render($page['top_header']); } ?>
       </div> <!-- /container -->
     </div> <!-- /top-header-single-region -->
